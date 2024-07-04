674382062f75f9d97e120e92dd9439f7f4c1f3e7
Francesco Casetta <francesco.casetta@gmail.com>
2024-07-04 08:18:10 +0200

    decorators and loaders

--- src/components/task-list/task-list.stories.tsx
+++ src/components/task-list/task-list.stories.tsx
@@ -1,10 +1,33 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { TaskList } from './task-list';
+import { TaskListProvider } from './task-list-context';
 
-const meta = {
-  title: 'Components/TaskList',
+const meta: Meta<typeof TaskList> = {
   component: TaskList,
-} as Meta<typeof TaskList>;
+  decorators: [
+    (Story, context) => {
+      return (
+        <TaskListProvider tasks={context.loaded.tasks}>
+          <Story />;
+        </TaskListProvider>
+      );
+    },
+  ],
+  loaders: [
+    async () => {
+      const tasks = await fetch('https://jsonplaceholder.typicode.com/todos').then((res) =>
+        res.json(),
+      );
+      return { tasks };
+    },
+  ],
+  title: 'Components/TaskList',
+};
 
 export default meta;
+
 type Story = StoryObj<typeof TaskList>;
+
+export const Default: Story = {
+  args: {},
+};
